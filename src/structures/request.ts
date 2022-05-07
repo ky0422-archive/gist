1155db00a6adfe9e5af079d5f5fae948fe65a54c
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 22:37:12 +0900

    Update request.ts

--- src/structures/request.ts
+++ src/structures/request.ts
@@ -1,12 +1,20 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, Method } from "axios";
 
 export default async <T>(
     url: string,
+    token: string,
+    method: Method,
     options: AxiosRequestConfig
 ): Promise<T> => {
     return await axios
         .request<T>({
             url,
+            method,
+            headers: {
+                "content-type": "application/json",
+                accept: "application/vnd.github.v3+json",
+                Authorization: `Bearer ${token}`,
+            },
             ...options,
         })
         .then((response) => {
