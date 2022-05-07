ad35d63809f81aabdba9cded8c810e55fa2c0b70
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 22:19:57 +0900

    Create request.js

---
+++ dist/src/structures/request.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+exports.default = async (url, options) => {
+    await axios_1.default.request({
+        url,
+        ...options
+    }).then((response) => {
+        return Promise.resolve(response.data);
+    }).catch((error) => {
+        return Promise.reject(error);
+    });
+};
