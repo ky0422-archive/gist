2adb00d666d48ebdcc59cd3209e107bc3466e923
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 23:31:55 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ export default class Gist implements IGist {
     public readonly token: string;
 
     constructor(token: string) {
+        if (!token || token === "token") {
+            throw new Error("Token required");
+        }
         this.token = token;
     }
 
