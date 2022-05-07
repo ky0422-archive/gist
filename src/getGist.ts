e445f4ccdda6d7664a06c370702eea59626a94c9
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 23:29:20 +0900

    Create getGist.ts

---
+++ src/getGist.ts
@@ -0,0 +1,17 @@
+import request from "./structures/request";
+import { GistResponse } from "./types";
+
+export default async (id: string, token: string): Promise<GistResponse> => {
+    return await request<GistResponse>(
+        `https://api.github.com/gists/${id}`,
+        token,
+        "GET",
+        {}
+    )
+        .then((response) => {
+            return Promise.resolve(response);
+        })
+        .catch((err) => {
+            return Promise.reject(err);
+        });
+};
