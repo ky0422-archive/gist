37dc33f9e47afa1642f35bdefd6a0b77f456f0ae
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 22:31:36 +0900

    Update createGist.js

--- dist/src/createGist.js
+++ dist/src/createGist.js
@@ -5,23 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const request_1 = __importDefault(require("./structures/request"));
 exports.default = async (files, description, token, options) => {
-    return await (0, request_1.default)('https://api.github.com/gists', {
-        method: 'POST',
+    return await (0, request_1.default)("https://api.github.com/gists", {
+        method: "POST",
         headers: {
-            'content-type': 'application/json',
-            'accept': 'application/vnd.github.v3+json',
-            'Authorization': `Bearer ${token}`,
+            "content-type": "application/json",
+            accept: "application/vnd.github.v3+json",
+            Authorization: `Bearer ${token}`,
         },
         data: {
             description: description,
             files: {
-                ...files
+                ...files,
             },
             public: options?.secret ? false : true,
-        }
-    }).then(res => {
-        return Promise.resolve(res);
-    }).catch(err => {
+        },
+    })
+        .then(response => {
+        return Promise.resolve(response);
+    })
+        .catch((err) => {
         return Promise.reject(err);
     });
 };
