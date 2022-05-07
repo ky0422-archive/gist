300a5969f334f536599560177633bca9a1286c69
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 22:37:06 +0900

    Update request.js

--- dist/src/structures/request.js
+++ dist/src/structures/request.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
-exports.default = async (url, options) => {
+exports.default = async (url, token, method, options) => {
     return await axios_1.default
         .request({
         url,
+        method,
+        headers: {
+            "content-type": "application/json",
+            accept: "application/vnd.github.v3+json",
+            Authorization: `Bearer ${token}`,
+        },
         ...options,
     })
         .then((response) => {
