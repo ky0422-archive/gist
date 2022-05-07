83cb5046ef8568e1a7cde36432832c6c28a90505
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 23:29:23 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { GistFile, GistOptions, GistResponse, IGist } from "./types";
 import createGist from "./createGist";
+import deleteGist from "./deleteGist";
+import getGist from "./getGist";
 
 export default class Gist implements IGist {
     public readonly token: string;
@@ -16,11 +18,11 @@ export default class Gist implements IGist {
         return createGist(files, description, this.token, options);
     }
 
-    public delete(id: string): Promise<any> {
-        throw new Error("Method not implemented.");
+    public delete(id: string): Promise<{}> {
+        return deleteGist(id, this.token);
     }
 
-    public get(id: string): Promise<any> {
-        throw new Error("Method not implemented.");
+    public get(id: string): Promise<GistResponse> {
+        return getGist(id, this.token);
     }
 }
