fc3c8cbbf3c598e789ecd6a4c4899f9229106587
tsukiroku <tsukiroku@yahoo.com>
2022-05-07 23:29:18 +0900

    Create deleteGist.ts

---
+++ src/deleteGist.ts
@@ -0,0 +1,19 @@
+import request from "./structures/request";
+
+export default async (
+    id: string,
+    token: string
+): Promise<{}> => {
+    return await request<{}>(
+        `https://api.github.com/gists/${id}`,
+        token,
+        "DELETE",
+        {}
+    )
+        .then((response) => {
+            return Promise.resolve(response);
+        })
+        .catch((err) => {
+            return Promise.reject(err);
+        });
+};
