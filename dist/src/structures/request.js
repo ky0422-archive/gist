d775459cb1a123dd2186eed4a505bd44c241e568
tsukiroku <tsukiroku@yahoo.com>
2022-05-08 16:33:47 +0900

    Update request.js

--- dist/src/structures/request.js
+++ dist/src/structures/request.js
@@ -4,20 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
+const http_status_1 = __importDefault(require("./http_status"));
 exports.default = async (url, token, method, options) => {
     return await axios_1.default
         .request({
         url,
         method,
         headers: {
-            "content-type": "application/json",
-            accept: "application/vnd.github.v3+json",
+            'content-type': 'application/json',
+            accept: 'application/vnd.github.v3+json',
             Authorization: `Bearer ${token}`,
         },
         ...options,
     })
         .then((response) => {
-        return Promise.resolve(response.data);
+        return Promise.resolve({
+            data: response.data,
+            status: (0, http_status_1.default)(response.status),
+        });
     })
         .catch((error) => {
         return Promise.reject(error);
