72906d900c48fb30ab4e2d21f7505ff1db5d9cd6
tsukiroku <tsukiroku@yahoo.com>
2022-05-08 16:34:24 +0900

    Update deleteGist.js

--- dist/src/deleteGist.js
+++ dist/src/deleteGist.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const request_1 = __importDefault(require("./structures/request"));
 exports.default = async (id, token) => {
-    return await (0, request_1.default)(`https://api.github.com/gists/${id}`, token, "DELETE", {})
+    return await (0, request_1.default)(`https://api.github.com/gists/${id}`, token, 'DELETE', {})
         .then((response) => {
-        return Promise.resolve(response);
+        return Promise.resolve({
+            status: response.status,
+        });
     })
         .catch((err) => {
         return Promise.reject(err);
