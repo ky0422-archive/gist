79e7988e9a78f3b0788de935347bdf1c0d4adc7d
tsukiroku <tsukiroku@yahoo.com>
2022-05-08 16:34:10 +0900

    Update getGist.js

--- dist/src/getGist.js
+++ dist/src/getGist.js
@@ -5,9 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const request_1 = __importDefault(require("./structures/request"));
 exports.default = async (id, token) => {
-    return await (0, request_1.default)(`https://api.github.com/gists/${id}`, token, "GET", {})
+    return await (0, request_1.default)(`https://api.github.com/gists/${id}`, token, 'GET', {})
         .then((response) => {
-        return Promise.resolve(response);
+        return Promise.resolve({
+            data: response.data,
+            status: response.status,
+        });
     })
         .catch((err) => {
         return Promise.reject(err);
