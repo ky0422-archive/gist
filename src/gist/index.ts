a2723858f158407b8024ae3c274fbdbced73c4d3
tsukiroku <tsukiroku@yahoo.com>
2022-06-06 20:28:54 +0900

    Update index.ts

--- src/gist/index.ts
+++ src/gist/index.ts
@@ -1,5 +1,6 @@
 import _create from './create';
 import _delete from './delete';
 import _get from './get';
+import _update from './update';
 
-export { _create, _delete, _get };
+export { _create, _delete, _get, _update };
