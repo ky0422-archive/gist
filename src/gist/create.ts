cd1004c5a60e9f97c012984bdfa69e439311a59f
tsukiroku <tsukiroku@yahoo.com>
2022-06-06 20:03:45 +0900

    Create create.ts

---
+++ src/gist/create.ts
@@ -0,0 +1,20 @@
+import request from '../structures/request';
+import { GistFile, GistOptions, GistResponse, ReqRet } from '../types';
+
+export default async (
+    files: GistFile,
+    description: string,
+    token: string,
+    options?: GistOptions
+): Promise<ReqRet<GistResponse>> =>
+    await request<GistResponse>('https://api.github.com/gists', token, 'POST', {
+        data: {
+            description: description,
+            files: {
+                ...files,
+            },
+            public: options?.secret ? true : false,
+        },
+    })
+        .then((response) => Promise.resolve(response))
+        .catch((err) => Promise.reject(err));
