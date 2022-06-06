b38942ec6a14b47109b88fa641afc090b0ca8ae8
tsukiroku <tsukiroku@yahoo.com>
2022-06-06 20:03:47 +0900

    Create delete.ts

---
+++ src/gist/delete.ts
@@ -0,0 +1,11 @@
+import request from '../structures/request';
+import { ReqRet } from '../types';
+
+export default async (id: string, token: string): Promise<ReqRet<{}>> =>
+    await request<{}>(`https://api.github.com/gists/${id}`, token, 'DELETE', {})
+        .then((response) =>
+            Promise.resolve({
+                status: response.status,
+            })
+        )
+        .catch((err) => Promise.reject(err));
